616d6293e6fec677d1941bf31f0adcc9730804c8
tqt97 <kutuanonline199@gmail.com>
2023-12-07 16:09:11 +0700

    Class properties

--- Chapter-1/Product.php
+++ Chapter-1/Product.php
@@ -1,11 +1,20 @@
 <?php
 
 declare(strict_types=1);
-
 class Product
 {
+    public string $name = "Product 1";
+    public int $price = 100;
 }
 
+// initialize object
 $p1 = new Product();
 
-var_dump($p1);
+// access properties
+print $p1->name . "\n";
+print $p1->price . "\n";
+
+// change properties
+$p1->price = 200;
+// access properties
+print $p1->price . "\n";
